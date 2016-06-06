48bafa8425fc10ff623dc5ac5ec076898ff00b11
Alice Frumin <alice@aghstrategies.com>
2016-06-06 17:09:25 -0600

    packery

--- js/drag.js
+++ js/drag.js
@@ -1,7 +1,5 @@
-$(function () { //DOM Ready
-  $('.gridster ul').gridster({
-    widget_margins: [10, 10],
-    widget_base_dimensions: [140, 140]
-  });
-
+$('.grid').packery({
+  // options
+  itemSelector: '.grid-item',
+  gutter: 10,
 });
