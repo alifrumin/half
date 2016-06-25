f9a3022ded48353bb8f0dcd278008a597df84c5f
Alice Frumin <alifrumin@yahoo.com>
2016-06-25 00:16:34 -0400

    js iterates thru rows

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
-(function () {
+$( document ).ready(function() {
   console.log("linked script.js");
   $("tr").each(function(){
-    var i = 0;
-    $(".miles").each(function(){
-      i = i + $(this).val();
-    });
-    $(".total").html(i);
-    console.log(i);
+    console.log("row");
   });
-})();
+});
