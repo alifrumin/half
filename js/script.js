206332df272262a0374f3b7892f1207f4b5618fa
Alice Frumin <alifrumin@yahoo.com>
2016-06-24 23:35:33 -0400

    semicolins

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 (function () {
   console.log("linked script.js");
-  $(".miles").each(function(){
-    $(this).addclass("foo");
-    console.log("rt");
+  $("tr").each(function(){
     var i = 0;
-    $("span.miles").each(function(){
+    $(".miles").each(function(){
       i = i + $(this).val();
-    })
+    });
     $(".total").html(i);
     console.log(i);
-  })
+  });
 })();
