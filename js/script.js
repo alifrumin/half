d3f56b8bab178956002cc9863371346016d57f50
Alice Frumin <alifrumin@yahoo.com>
2016-06-29 21:13:22 -0400

    adding all miles together

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
+// $( document ).ready(function() {
+//   console.log("linked script.js");
+//   $("tr").each(function(){
+//     if ($(".miles").parent().is(this)) {
+//       $(".miles").each(function(){
+//         var miles = parseInt($(".miles").text());
+//         console.log(miles);
+//         var i = 0;
+//         i = i + $(".miles").val();
+//         $(".total").html(i);
+//     });
+//     }
+//   })
+// });
+
 $( document ).ready(function() {
-  console.log("linked script.js");
-  $("tr").each(function(){
-    console.log("row");
+  i = 0;
+  $(".miles").each(function(){
+      var week = parseInt($(this).text());
+      if (week > 0) {
+        i = i + week;
+        console.log(i);
+      }
   });
+  // var miles = parseInt($(".miles").text());
+  // console.log(miles);
+  // var i = 0;
+  // i = i + miles;
+  // $(".total").html(i);
 });
